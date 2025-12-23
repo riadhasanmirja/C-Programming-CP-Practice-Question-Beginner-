78db9da0447958f9fa4f732fc1642d8ced3fc1de
MD. Riad Hasan Mirja <riadrikto0357@gmail.com>
2025-12-23 19:06:00 +0600

    Create 3 integer value Sum.c

---
+++ 3 integer value Sum.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    
+    int a;
+    int b;
+    int c;
+    
+    printf("Enter your integer value a :");
+    scanf("%d",&a);
+    
+    printf("Enter your integer value b :");
+    scanf("%d",&b);
+    
+    printf("Enter your integer value :");
+    scanf("%d",&c);
+    
+    int Sum = a + b + c;
+    printf("Sum is :%d",Sum);
+    
+    return 0;
+}
