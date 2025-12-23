77d598ed323ab624eaf9db7437f6d2fd3dbc6eac
MD. Riad Hasan Mirja <riadrikto0357@gmail.com>
2025-12-23 13:30:12 +0600

    Create Sum two integer.c

---
+++ Sum two integer.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+   
+   int a;
+   int b;
+   
+   printf("Enter your integer valu a :");
+   scanf("%d",&a);
+   
+   printf("Enter your integer valu b :");
+   scanf("%d",&b);
+
+   int Sum = a + b;
+   printf("sum is :%d",Sum);
+
+    return 0;
+}
