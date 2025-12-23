42c8a265ead8382aa26e44cd07b7b9e76dc1345d
MD. Riad Hasan Mirja <riadrikto0357@gmail.com>
2025-12-23 13:59:13 +0600

    Create Square.c

---
+++ Square.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+   
+   int a;
+   
+   printf("Enter your integer value a :");
+   scanf("%d",&a);
+  
+   int Square = a*a;
+   printf("Square value is :%d",Square);
+   
+   return 0;
+}
