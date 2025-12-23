3417201e207681030d722891262695835284fa6f
MD. Riad Hasan Mirja <riadrikto0357@gmail.com>
2025-12-23 13:43:58 +0600

    Update Print Character value.c

--- Print Character value.c
+++ Print Character value.c
@@ -5,10 +5,10 @@ int main() {
    char a;
    char b;
    
-   printf("Enter your Character valu a :");
+   printf("Enter your Character value a :");
    scanf("%c",&a);
    
-   printf("Enter your Character valu b :");
+   printf("Enter your Character value b :");
    scanf(" %c",&b);      //space before %c
 
     return 0;
