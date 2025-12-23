fc606b9d25ae3203f286be388115450f4f5232a5
MD. Riad Hasan Mirja <riadrikto0357@gmail.com>
2025-12-23 14:00:08 +0600

    Create Cube.c

---
+++ Cube.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+   
+   int a;
+   
+   printf("Enter your integer value a :");
+   scanf("%d",&a);
+  
+   int Cube = a*a*a;
+   printf("Square value is :%d",Cube);
+   
+   return 0;
+}
