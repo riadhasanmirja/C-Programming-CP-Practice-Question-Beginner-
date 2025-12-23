acc471031c3aa00e45140f7ff2f77cc4dff3b941
MD. Riad Hasan Mirja <riadrikto0357@gmail.com>
2025-12-23 13:35:25 +0600

    Create Sum Floating two valu.c

---
+++ Sum Floating two valu.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+   
+   float a;
+   float b;
+   
+   printf("Enter your Floating valu a :");
+   scanf("%f",&a);
+   
+   printf("Enter your Floating valu b :");
+   scanf("%f",&b);
+
+   float Sum = a + b;
+   printf("sum is :%.2f",Sum);
+
+    return 0;
+}
