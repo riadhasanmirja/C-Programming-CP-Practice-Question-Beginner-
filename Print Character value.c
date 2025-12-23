292f5538d16ae7220e8ab21fa71c4fe6c44e86cc
MD. Riad Hasan Mirja <riadrikto0357@gmail.com>
2025-12-23 13:42:01 +0600

    Create Print Character value.c

---
+++ Print Character value.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+   
+   char a;
+   char b;
+   
+   printf("Enter your Character valu a :");
+   scanf("%c",&a);
+   
+   printf("Enter your Character valu b :");
+   scanf(" %c",&b);      //space before %c
+
+    return 0;
+}
